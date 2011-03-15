6fab6219cfecdb253ae5d933b5797daefa5b3760
Hendrik Kunert <hendrik-kunert@freenet.de>
2011-03-15 20:42:22 +0100

    Licence, readme

--- wplookup.h
+++ wplookup.h
@@ -1,23 +1,24 @@
 /*
- * Wikipedia Lookup - A third-party Pidgin plug-in which offers 
+ *  Wikipedia Lookup - A third-party Pidgin plug-in which offers 
  *					  you the possibility to look up received and
  *					  typed words on Wikipedia.
  *
- * Copyright (C) 2011 Hendrik Kunert kunerd@users.sourceforge.net
+ *  Copyright (C) 2011 Hendrik Kunert kunerd@users.sourceforge.net
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ *  This file is part of wplookup.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  wplookup is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1301, USA.
+ *  Foobar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with wplookup.  If not, see <http://www.gnu.org/licenses/>.
  */
  
 #ifndef _PIDGIN_LATEX_H_
